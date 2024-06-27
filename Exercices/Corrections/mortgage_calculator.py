44be70c326d986a7b45c536a7b78fda8c731002d
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-06-27 06:05:14 +0200

    Adding borrowing capacity

--- Exercices/Corrections/mortgage_calculator.py
+++ Exercices/Corrections/mortgage_calculator.py
@@ -10,10 +10,20 @@ def get_remaining_principal(mortgage: float, monthly_instalment: float, time_in_
 
 def monthly_instalment_scenario():
     mortgage = 200_000
-    nb_months = 240
-    rate = 0.03
+    nb_months = 20*12
+    rate = 0.037
     monthly_instalment = symbols("monthly_instalment")
     print("Mensualité: ", solve(get_remaining_principal(mortgage, monthly_instalment, nb_months, rate)))
 
+
+def borrowing_capacity_scenario():
+    max_monthly_instalment = 2000
+    nb_months = 25*12
+    rate = 0.04
+    max_mortgage = symbols("max_mortgage")
+    print("Capacité d'emprunt: ", solve(get_remaining_principal(max_mortgage, max_monthly_instalment, nb_months, rate)))
+
+
 if __name__ == "__main__":
     monthly_instalment_scenario()
+    borrowing_capacity_scenario()
