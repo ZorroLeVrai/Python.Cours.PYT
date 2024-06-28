f8bb0ece0e8f406877856dc224c7c6f32124c8fb
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-06-28 11:06:43 +0200

    MAJ exercice insertion de fichier

--- Cheat sheet/functions.py
+++ Cheat sheet/functions.py
@@ -1,6 +1,7 @@
 """
 Les fonctions
 """
+from collections.abc import Callable
 
 #fonction qui ne prend aucun paramètre
 def do_nothing():
@@ -20,27 +21,89 @@ Les fonctions lambda
 
 # fonction lambda avec un argument
 incrementer = lambda x: x+1
-print(incrementer(5))   #6
+# print(incrementer(5))   #6
 
 # fonction lambda avec zero argument
 obtenir_zero = lambda : 0
-print(obtenir_zero())   #0
+# print(obtenir_zero())   #0
 
 # fonction lambda avec 2 arguments
 additionner = lambda a,b: a + b
-print(additionner(1,2)) #3
+# print(additionner(1,2)) #3
 
 """
 Les fonctions récursives
 """
 
 #fonction recursive
-def factorial(n: int):
+def factorial(n: int) -> int:
     if n < 0:
         raise ValueError("Le paramètre n doit être supérieur ou égal à zéro")
     if n == 0:
         return 1
     return n * factorial(n-1)
+
+"""
+Les fonctions locales
+"""
+def imprimer_factorial(n: int) -> None:
+    def imprimer(valeur: int) -> None:
+        print(f"Factoriel de {n} = {valeur}")
+
+    resultat = factorial(n)
+    imprimer(resultat)
+
+# imprimer_factorial(5)  #120
+
+"""
+Fonction prenant d'autres fonctions en paramètre
+"""
+def calculer_imprimer(calculer: Callable[[], int], imprimer: Callable[[int], None]) -> None:
+    resultat = calculer()
+    imprimer(resultat)
+
+# calculer_imprimer(lambda : factorial(5), lambda solution: print(f"Résultat {solution}"))    #Résultat 120
+
+"""
+Closures
+"""
+def illustrer_closure(nombre1: float, nombre2: float) -> None:
+    calculer_v1 = lambda : nombre1 + nombre2
+    def calculer_v2():
+        return nombre1 + nombre2
+
+    print(calculer_v1())
+    print(calculer_v2())
+
+illustrer_closure(5, 2);
+
+def illustrer_closure_pb_reference():
+    my_functions: list[Callable[[], None]] = list()
+
+    for index in range(1, 9):
+        my_functions.append(lambda: print(index))
+
+    #execution des fonctions
+    for func in my_functions:
+        func()
+
+# illustrer_closure_pb_reference()
+
+def illustrer_resolution_pb_reference():
+    def creer_fonction_imprimer(index: int) -> None:
+        imprimer = lambda : print(index)
+        return imprimer
+
+    my_functions: list[Callable[[], None]] = list()
+
+    for index in range(1, 9):
+        my_functions.append(creer_fonction_imprimer(index))
+
+    #execution des fonctions
+    for func in my_functions:
+        func()
+
+# illustrer_resolution_pb_reference()
 
 """
 Les fonctions generator
@@ -53,4 +116,4 @@ def ma_fonction_range(initial_value: int, max_value_excluded: int, step: int):
         yield current_value
         current_value += step
 
-print(list(ma_fonction_range(1, 10, 2)))    #[1, 3, 5, 7, 9]
+# print(list(ma_fonction_range(1, 10, 2)))    #[1, 3, 5, 7, 9]
--- Exercices/Corrections/insert_exercise.py
+++ Exercices/Corrections/insert_exercise.py
@@ -1,51 +1,107 @@
+"""
+Module utilisé pour insérer un fichier Python dans le répertoire de correction des exercices
+"""
+
 from collections.abc import Callable
 import os
 from os.path import exists
 
-def generate_file_name(index: int) -> str:
-    return f"ex_{index:03}.py"
 
+class FileHandler:
+    """
+    Classe pour la gestion des fichiers
+    Attributes:
+        execute_action : bool
+            Si True effectue les opérations sur les fichiers sinon ne modifie pas les fichiers
+    """
 
-def create_file(file_name: str) -> None:
-    print(f"create file {file_name}")
-    #open(file_name, 'a').close()
+    def __init__(self, execute_action: bool):
+        self.execute_action = execute_action
 
+    def create_file(self, file_name: str) -> bool:
+        print(f"create file {file_name}")
+        if self.execute_action:
+            try:
+                with open(file_name, 'a'):
+                    pass
+            except OSError as os_error:
+                print(os_error)
+                return False
+        return True
 
-def rename_file_factory(old_name: str, new_name: str) -> Callable[[], None]:
-    def rename_file() -> None:
-        #os.rename(old_name, new_name)
+    def rename_file(self, old_name: str, new_name: str) -> bool:
         print(f"{old_name} => {new_name}")
-    
-    return rename_file
+        if self.execute_action:
+            try:
+                os.rename(old_name, new_name)
+            except OSError as os_error:
+                print(os_error)
+                return False
+        return True
+
+    def file_exists(self, file_name: str) -> bool:
+        """ Vérifie si le fichier file_name existe """
+        return exists(file_name)
 
 
-def fix_file_name_collisions(index: int, file_name: str) -> None:
-    def generate_rename_actions() -> list[Callable[[], None]]:
-        action_list: list[Callable[[], None]] = []
-        previous_name = file_name
-        current_name = file_name
-        current_index = index
-        while exists(current_name):
-            current_index += 1
-            current_name = generate_file_name(current_index)
-            action_list.append(rename_file_factory(previous_name, current_name))
-            previous_name = current_name
-        return action_list
-    
-    def execute_actions_in_reverse(actions: list[Callable[[], None]]):
-        actions.reverse()
-        for action in actions:
-            action()
+class ExerciseInsertor:
+    """
+    Classe permettant la création d'un fichier Python avec le bon index.
+    Les autres fichiers sont décalés si besoin
+    Attributes:
+        file_handler : FileHandler
+            prend en charge la gestion des fichiers
+    """
 
-    actions = generate_rename_actions()
-    execute_actions_in_reverse(actions)
+    def __init__(self, file_handler: FileHandler, file_format: str):
+        self.file_handler = file_handler
+        self.file_format = file_format
 
 
-def insert_exercise(index: int) -> None:
-    file_name = generate_file_name(index)
-    if exists(file_name):
-        fix_file_name_collisions(index, file_name)
-    create_file(file_name)
+    def generate_file_name(self, index: int) -> str:
+        return self.file_format.format(index)
 
-os.chdir(r"D:\Users\Amine\Prgm\Python\Python.Cours.PYT\Exercices\Corrections")
-insert_exercise(7)
+    def rename_file_factory(self, old_name: str, new_name: str) -> Callable[[], bool]:
+        def rename_file() -> bool:
+            return self.file_handler.rename_file(old_name, new_name)
+
+        return rename_file
+
+    def fix_file_name_collisions(self, index: int, file_name: str) -> bool:
+        def generate_rename_actions() -> list[Callable[[], bool]]:
+            action_list: list[Callable[[], bool]] = []
+            previous_name = file_name
+            current_name = file_name
+            current_index = index
+            while self.file_handler.file_exists(current_name):
+                current_index += 1
+                current_name = self.generate_file_name(current_index)
+                action_list.append(self.rename_file_factory(previous_name, current_name))
+                previous_name = current_name
+            return action_list
+
+        def execute_actions_in_reverse(actions: list[Callable[[], bool]]) -> bool:
+            actions.reverse()
+            for action in actions:
+                is_action_done = action()
+                if not is_action_done:
+                    return False
+            return True
+
+        actions = generate_rename_actions()
+        return execute_actions_in_reverse(actions)
+
+    def insert_exercise(self, index: int) -> bool:
+        file_name = self.generate_file_name(index)
+        if self.file_handler.file_exists(file_name):
+            is_collision_fixed = self.fix_file_name_collisions(index, file_name)
+            if not is_collision_fixed:
+                return False
+        self.file_handler.create_file(file_name)
+        return True
+
+
+if __name__ == "__main__":
+    os.chdir(r"D:\Users\Amine\Prgm\Python\Python.Cours.PYT\Exercices\Corrections")
+    exerciceInsertor = ExerciseInsertor(FileHandler(False), "ex_{0:03}.py")
+    exerciceInsertor.insert_exercise(7)
