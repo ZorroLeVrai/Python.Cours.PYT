aa8b41d368c2065b5c67530a14fb7106e7174973
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-09-19 10:47:30 +0200

    Update ex_011.py

--- Exercices/Corrections/ex_011.py
+++ Exercices/Corrections/ex_011.py
@@ -1,16 +1,25 @@
 def inverser_chaine(text: str) -> str:
     chaine_inversee = ""
-    for index in range(len(text)-1, -1, -1):
+    longeur_chaine = len(text)
+    for index in range(longeur_chaine-1, -1, -1):
         chaine_inversee += text[index]
 
     return chaine_inversee
 
 def inverser_chaine_v2(text: str) -> str:
+    chaine_inversee = ""
+    longeur_chaine = len(text)
+    for index in range(-1, -1*longeur_chaine-1, -1):
+        chaine_inversee += text[index]
+
+    return chaine_inversee
+
+def inverser_chaine_v3(text: str) -> str:
     chars = list(text)
     chars.reverse()
     return "".join(chars)
 
-def inverser_chaine_v3(text: str) -> str:
+def inverser_chaine_v4(text: str) -> str:
     return text[::-1]
 
 
@@ -19,3 +28,4 @@ if __name__ == "__main__":
     print(inverser_chaine(texte))
     print(inverser_chaine_v2(texte))
     print(inverser_chaine_v3(texte))
+    print(inverser_chaine_v4(texte))
