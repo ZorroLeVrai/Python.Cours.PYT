009b3f7aad0a712f399fe12eef13f9154b658901
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-09-20 05:40:51 +0200

    Ajout d'une démo pour fusionner des fichiers

---
+++ Demos/Fusionner_fichiers/fusionner.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+import glob
+
+# Spécifiez le répertoire où chercher les fichiers
+repertoire_source = r".\Data"
+# Spécifiez le format des fichiers à inclure
+format_fichiers_source = "villes_*.csv"
+# Spécifier l'emplacement du fichier cible
+fichier_cible = r".\Output\villes.csv"
+
+#def fichier_vers_dataframe()
+
+# Utiliser glob pour trouver tous les fichiers correspondant au modèle "villes_*.csv"
+fichiers_csv = glob.glob(os.path.join(repertoire_source, format_fichiers_source))
+
+df_liste = [pd.read_csv(fichier) for fichier in fichiers_csv]
+#df_liste = list(map(lambda fichier:pd.read_csv(fichier), fichiers_csv))
+
+#fusionner la liste des dataframes
+df_fusionne = pd.concat(df_liste, ignore_index=True)
+
+# Sauvegarder le DataFrame fusionné dans un fichier CSV
+df_fusionne.to_csv(fichier_cible, index=False)
+
+print("Les DataFrames ont été fusionnés et sauvegardés avec succès.")
--- test.py
+++ test.py
@@ -1,9 +1,13 @@
-adresse = "Puteaux"
-texte = f"""Début "{adresse}" 
-'{adresse}' Fin"""
+import pandas as pd
 
+# Lire les fichiers CSV
+df1 = pd.read_csv('fichier1.csv')
+df2 = pd.read_csv('fichier2.csv')
 
-txt1 = "texte"
-txt1 = 'texte'
-txt1 = """texte"""
-print(texte)
+# Fusionner les deux DataFrames (par défaut, axis=0 pour concaténer verticalement)
+df_fusionne = pd.concat([df1, df2], ignore_index=True)
+
+# Sauvegarder le DataFrame fusionné dans un fichier CSV
+df_fusionne.to_csv('fichier_fusionne.csv', index=False)
+
+print("Les DataFrames ont été fusionnés et sauvegardés avec succès.")
