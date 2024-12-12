a209a2fc761796d80312110fbd08e0fe46b0ce06
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-12-12 04:23:46 +0100

    Update ex_036.py

--- Exercices/Corrections/ex_036.py
+++ Exercices/Corrections/ex_036.py
@@ -1,28 +1,27 @@
 from itertools import combinations
 
 class Element:
-    def __init__(self, valeur: int, postfixe: str, infixe: str):
+    def __init__(self, valeur: int, infixe: str):
         self.valeur = valeur
-        self.postfixe = postfixe
         self.infixe = infixe
 
     def __str__(self):
-        return f"[{self.postfixe}:{self.valeur}]"
+        return f"[{self.infixe}:{self.valeur}]"
     
     def __repr__(self):
         return self.__str__()
     
     def __add__(self, other):
-        return Element(self.valeur + other.valeur, f"{self.postfixe} {other.postfixe} +", f"({self.infixe}+{other.infixe})")
+        return Element(self.valeur + other.valeur, f"({self.infixe}+{other.infixe})")
 
     def __sub__(self, other):
-        return Element(self.valeur - other.valeur, f"{self.postfixe} {other.postfixe} -", f"({self.infixe}-{other.infixe})")
+        return Element(self.valeur - other.valeur, f"({self.infixe}-{other.infixe})")
 
     def __mul__(self, other):
-        return Element(self.valeur * other.valeur, f"{self.postfixe} {other.postfixe} *", f"({self.infixe}*{other.infixe})")
+        return Element(self.valeur * other.valeur, f"({self.infixe}*{other.infixe})")
 
     def __truediv__(self, other):
-        return Element(self.valeur // other.valeur, f"{self.postfixe} {other.postfixe} /", f"({self.infixe}/{other.infixe})")
+        return Element(self.valeur // other.valeur, f"({self.infixe}//{other.infixe})")
     
     def __ge__(self, other):
         return self.valeur >= other.valeur
@@ -51,7 +50,10 @@ class MeilleurSolution:
 
 
 def creer_element(valeur: int):
-    return Element(valeur, str(valeur), str(valeur))
+    return Element(valeur, str(valeur))
+
+def copier_elements_sauf_index(elements: list[Element], index_a_ignorer: list[int]) -> list[Element]:
+    return [elements[i] for i in range(len(elements)) if i not in index_a_ignorer]
 
 def calculer(element_gauche: Element, element_droite: Element, operateur: str):
     match operateur:
@@ -69,8 +71,7 @@ def calculer(element_gauche: Element, element_droite: Element, operateur: str):
 def simuler_operation(elements: list[Element], position_gauche: int, position_droite: int, operateur: str, cible: int, meilleur_solution: MeilleurSolution) -> tuple[list[Element], MeilleurSolution]:
     nouvel_element = calculer(elements[position_gauche], elements[position_droite], operateur)
     meilleur_solution = meilleur_solution.retourner_meilleur(nouvel_element, cible)
-    elements_copie = elements.copy()
-    del elements_copie[position_gauche, position_droite]
+    elements_copie = copier_elements_sauf_index(elements, [position_gauche, position_droite])
     elements_copie.append(nouvel_element)
     return elements_copie, meilleur_solution
     
@@ -99,10 +100,15 @@ def resoudre(elements: list[Element], cible: int) -> MeilleurSolution:
             solutions.append(resoudre_loc(_elements, _meilleur_solution))
             # Diviser
             if op_gauche >= op_droite:
-                _elements, _meilleur_solution = simuler_operation(elements, op_gauche, op_droite, "/", cible, meilleur_solution)
+                numerateur = op_gauche
+                denominateur = op_droite
             else:
-                _elements, _meilleur_solution = simuler_operation(elements, op_droite, op_gauche, "/", cible, meilleur_solution, meilleur_distance)
-            solutions.append(resoudre_loc(_elements, _meilleur_solution))
+                numerateur = op_droite
+                denominateur = op_gauche
+
+            if elements[denominateur].valeur != 0:
+                _elements, _meilleur_solution = simuler_operation(elements, numerateur, denominateur, "/", cible, meilleur_solution)
+                solutions.append(resoudre_loc(_elements, _meilleur_solution))
 
         return min(solutions, key=lambda x: x.distance)
 
@@ -115,5 +121,5 @@ def resoudre(elements: list[Element], cible: int) -> MeilleurSolution:
         return None
 
 
-meilleur_solution = resoudre([creer_element(10), creer_element(5)], 5)
+meilleur_solution = resoudre([creer_element(3), creer_element(100), creer_element(8), creer_element(8), creer_element(10), creer_element(6)], 683)
 print(meilleur_solution.solution)
